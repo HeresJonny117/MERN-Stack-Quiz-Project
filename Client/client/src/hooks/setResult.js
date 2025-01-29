a85fa4839046883a4646d0e3d23c805abf5bd947
Jonathan Hegarty <jhegarty968@gmail.com>
2025-01-29 18:53:23 -0500

    added set results js

---
+++ Client/client/src/hooks/setResult.js
@@ -0,0 +1,47 @@
+import { postServerData } from "../helper/helper";
+import * as Action from "../redux/result_reducer";
+
+
+export const pushAnswer = (result) => async (dispatch) => {
+    try {
+        await dispatch(Action.pushResultAction(result));
+    } catch (error) {
+        console.error("Error pushing answer:", error);
+    }
+};
+
+
+export const updateResult = (payload) => async (dispatch) => {
+    try {
+        dispatch(Action.updateResultAction(payload));
+    } catch (error) {
+        console.error("Error updating result:", error);
+    }
+};
+
+
+export const usePublishResult = (resultData) => {
+    const { result, username } = resultData;
+
+    const publishResult = async () => {
+        try {
+            
+            if (!result.length || !username) {
+                throw new Error("Couldn't get Result: Invalid result or username.");
+            }
+
+            const port = process.env.PORT || 5000;
+            const response = await postServerData(
+                `http://localhost:${port}/api/result`,
+                resultData
+            );
+
+            console.log("Result published:", response);
+        } catch (error) {
+            console.error("Error publishing result:", error);
+        }
+    };
+
+    
+    publishResult();
+};
