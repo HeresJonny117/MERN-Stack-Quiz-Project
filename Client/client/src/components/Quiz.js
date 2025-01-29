389570380b5c99c987ea9a8b55c2760c27d2da5c
Jonathan Hegarty <jhegarty968@gmail.com>
2025-01-29 17:12:04 -0500

    fixed code for some navigate issues

--- Client/client/src/components/Quiz.js
+++ Client/client/src/components/Quiz.js
@@ -5,52 +5,49 @@ import Questions from "./Questions";
 import { MoveNextQuestion, MovePrevQuestion } from "../hooks/FetchQuestion";
 import { PushAnswer } from "../hooks/setResult";
 
-export default function Quiz() {
-    const [check, setChecked] = useState(undefined);
-    const result = useSelector((state) => state.result.result);
-    const { queue, trace } = useSelector((state) => state.questions);
+const Quiz = () => {
+    const [selectedAnswer, setSelectedAnswer] = useState(undefined);
     const dispatch = useDispatch();
 
-    function onNext() {
+    const { result } = useSelector((state) => state.result);
+    const { queue, trace } = useSelector((state) => state.questions);
+
+    const handleNext = () => {
         if (trace < queue.length) {
             dispatch(MoveNextQuestion());
-            if (result.length <= trace) {
-                dispatch(PushAnswer(check));
-            }
+            if (result.length <= trace) dispatch(PushAnswer(selectedAnswer));
         }
-        setChecked(undefined);
-    }
+        setSelectedAnswer(undefined);
+    };
 
-    function onPrev() {
-        if (trace > 0) {
-            dispatch(MovePrevQuestion());
-        }
-    }
+    const handlePrev = () => {
+        if (trace > 0) dispatch(MovePrevQuestion());
+    };
 
-    function onChecked(check) {
-        setChecked(check);
-    }
+    const handleSelect = (answer) => {
+        setSelectedAnswer(answer);
+    };
 
-    if (result.length && result.length >= queue.length) {
-        return <Navigate to={"/result"} replace={true}></Navigate>;
+    if (result.length >= queue.length) {
+        return <Navigate to="/result" replace />;
     }
 
     return (
         <div className="container">
             <h1 className="title text-light">Quiz Application</h1>
-            <Questions onChecked={onChecked} />
+            <Questions onChecked={handleSelect} />
             <div className="grid">
-                {trace > 0 ? (
-                    <button className="btn prev" onClick={onPrev}>
+                {trace > 0 && (
+                    <button className="btn prev" onClick={handlePrev}>
                         Prev
                     </button>
-                ) : (
-                    <div></div>
                 )}
-                <button className="btn next" onClick={onNext}>
+                <button className="btn next" onClick={handleNext}>
                     Next
                 </button>
             </div>
         </div>
     );
-}
+};
+
+export default Quiz;
