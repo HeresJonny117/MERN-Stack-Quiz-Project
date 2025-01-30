445f774355b1d34b376250d0cc4e22b437b51c24
aquabarnabas <aquabarnabas@gmail.com>
2025-01-29 20:37:28 -0500

    fixed backend

--- Server/database/conn.js
+++ Server/database/conn.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
+require("dotenv").config;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-    });
+    console.log("MongoDB URI:", process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
   } catch (e) {
     console.error("Database Failed", e.message);
+    throw e;
   }
 };
 
-module.export = connectDB;
+module.exports = connectDB;
--- Server/server.js
+++ Server/server.js
@@ -5,7 +5,7 @@ const routes = require("./route/route");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use("/api", routes);
